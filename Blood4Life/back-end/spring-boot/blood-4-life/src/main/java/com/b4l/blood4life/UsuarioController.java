640e09ebb3ffe2fd58c443565c8325ba15659a1b
raphaelbachega <raphael.messias@bandtec.com.br>
2020-09-14 19:21:10 -0300

    rebuild request scripts postman

--- Blood4Life/back-end/spring-boot/blood-4-life/src/main/java/com/b4l/blood4life/UsuarioController.java
+++ Blood4Life/back-end/spring-boot/blood-4-life/src/main/java/com/b4l/blood4life/UsuarioController.java
@@ -12,24 +12,36 @@ public class UsuarioController {
 
     @PostMapping("/doador")
     public ResponseEntity logarDoador(@RequestParam String email, @RequestParam String senha){
-        if (email.equals("daniel.juncks@bandtec.com.br") && senha.equals("12345")){
-            autenticado = true;
-            return ResponseEntity.ok("Autenticado com sucesso!");
+
+        if(autenticado){
+            return ResponseEntity.badRequest().build();
         }
         else{
-            autenticado = false;
-            return ResponseEntity.status(404).build();
+            if (email.equals("daniel.juncks@bandtec.com.br") && senha.equals("12345")){
+                autenticado = true;
+                return ResponseEntity.ok("Autenticado com sucesso!");
+            }
+            else{
+                autenticado = false;
+                return ResponseEntity.status(404).build();
+            }
         }
     }
     @PostMapping("/adm")
     public ResponseEntity logarAdm(@RequestParam String email, @RequestParam String senha){
-        if (email.equals("blood4life@bandtec.com.br") && senha.equals("12345")){
-            autenticado = true;
-            return ResponseEntity.ok("Autenticado com sucesso!");
+
+        if(autenticado){
+            return ResponseEntity.badRequest().build();
         }
         else{
-            autenticado = false;
-            return ResponseEntity.status(404).build();
+            if (email.equals("blood4life@bandtec.com.br") && senha.equals("12345")){
+                autenticado = true;
+                return ResponseEntity.ok("Autenticado com sucesso!");
+            }
+            else{
+                autenticado = false;
+                return ResponseEntity.status(404).build();
+            }
         }
     }
 
