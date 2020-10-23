c0054b38c2eaac6fac3ffeb8bf67080178aad8b9
ranyery <ranyerysc@gmail.com>
2020-10-23 17:00:29 -0300

    feat(hospitalController): add new endpoints

--- Blood4Life/back-end/spring-boot/blood-4-life/src/main/java/com/b4l/blood4life/controladores/HospitalController.java
+++ Blood4Life/back-end/spring-boot/blood-4-life/src/main/java/com/b4l/blood4life/controladores/HospitalController.java
@@ -1,4 +1,63 @@
 package com.b4l.blood4life.controladores;
 
+import com.b4l.blood4life.dominios.Hospital;
+import com.b4l.blood4life.repositorios.HospitalRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/hospitais")
 public class HospitalController {
+
+    @Autowired
+    HospitalRepository hospitalRepository;
+
+    @GetMapping
+    public ResponseEntity getHospitais() {
+        return hospitalRepository.count() > 0
+                ? ResponseEntity.ok(hospitalRepository.findAll())
+                : ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity getHospitalById(@PathVariable Integer id) {
+        if (hospitalRepository.existsById(id)) {
+            return ResponseEntity.ok(hospitalRepository.findById(id));
+        }
+
+        return ResponseEntity.notFound().build();
+    }
+
+    @PostMapping
+    public ResponseEntity adicionarHospital(@RequestBody @Valid Hospital hospital) {
+        hospitalRepository.save(hospital);
+
+        return ResponseEntity.created(null).build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity editaHospital(@PathVariable Integer id,
+                                        @Valid @RequestBody Hospital hospital) {
+        if (hospitalRepository.existsById(id)){
+            hospital.setId(id);
+            hospitalRepository.save(hospital);
+            return ResponseEntity.ok().build();
+        }
+
+        return ResponseEntity.notFound().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity deletaHospital(@PathVariable Integer id) {
+        if (hospitalRepository.existsById(id)){
+            hospitalRepository.deleteById(id);
+            return ResponseEntity.ok().build();
+        }
+
+        return ResponseEntity.notFound().build();
+    }
+
 }
