1f7452a83d87d554b5bbe2a9e1e8c920076fb587
ranyery <ranyerysc@gmail.com>
2020-10-23 17:02:24 -0300

    fix(hospitalRepository): change method signature type

---
+++ Blood4Life/back-end/spring-boot/blood-4-life/src/main/java/com/b4l/blood4life/repositorios/HospitalRepository.java
@@ -0,0 +1,8 @@
+package com.b4l.blood4life.repositorios;
+
+import com.b4l.blood4life.dominios.Hospital;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface HospitalRepository extends JpaRepository<Hospital, Integer> {
+
+}
