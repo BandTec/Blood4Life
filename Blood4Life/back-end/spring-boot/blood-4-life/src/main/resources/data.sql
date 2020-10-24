28ae9ea38e4ffb4e2d6be6bcba86831dcb74b580
ranyery <ranyerysc@gmail.com>
2020-10-24 18:54:41 -0300

    fix(data.sql): change name attribute

--- Blood4Life/back-end/spring-boot/blood-4-life/src/main/resources/data.sql
+++ Blood4Life/back-end/spring-boot/blood-4-life/src/main/resources/data.sql
@@ -1,4 +1,4 @@
-insert into doador (id_doador,nome,email,senha, dt_nascimento, cpf, telefone,tipo_sanguineo,genero) values
+insert into doador (id,nome,email,senha, dt_nascimento, cpf, telefone,tipo_sanguineo,genero) values
 (null, 'Pedrin', 'pedrin@gmail.com', 'senha123', '2020-09-10', '48611558863', '11912345678', 'O+','M'),
 (null, 'João', 'joao@gmail.com', '123senha', '2020-10-01', '48688631155', '11912678345', '', 'F'),
 (null, 'Bachega', 'bachega@gmail.com', 'basenha', '2020-09-24', '24688631155', '11912678343', 'AB+', 'M'),
