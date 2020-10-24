c5f813b57fdea8436a9b4f145de30aaf5ce2b1e2
ranyery <ranyerysc@gmail.com>
2020-10-24 18:50:24 -0300

    feat: add user session validation

--- Blood4Life/back-end/spring-boot/blood-4-life/src/main/java/com/b4l/blood4life/controladores/DoadoresController.java
+++ Blood4Life/back-end/spring-boot/blood-4-life/src/main/java/com/b4l/blood4life/controladores/DoadoresController.java
@@ -3,13 +3,11 @@ package com.b4l.blood4life.controladores;
 import com.b4l.blood4life.dominios.Doador;
 import com.b4l.blood4life.repositorios.DoadoresRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Example;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import javax.swing.text.html.Option;
+import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
-import javax.websocket.server.PathParam;
 import java.util.List;
 import java.util.Optional;
 
@@ -20,41 +18,59 @@ public class DoadoresController {
     @Autowired
     private DoadoresRepository repository;
 
-    @GetMapping()
-    public ResponseEntity listar(@RequestParam(required = false) String tipoSanguineo) {
-        Doador doador = new Doador(tipoSanguineo);
-        List<Doador> doadoresFiltrados = repository.findAll(Example.of(doador));
-        if (tipoSanguineo == null) {
-            if (repository.count() > 0){
-                return ResponseEntity.ok(repository.findAll());
-            }else {
-                return ResponseEntity.noContent().build();
-            }
-        } else {
-            if (doadoresFiltrados.isEmpty()){
-                return ResponseEntity.noContent().build();
-            }else{
-                return ResponseEntity.ok(doadoresFiltrados);
-            }
+    @GetMapping
+    public ResponseEntity getDoadores(
+            HttpSession session
+    ) {
+        if (session.getAttribute("usuarioLogado") == null) {
+            return ResponseEntity.status(401).build();
         }
+
+        if (repository.count() > 0) {
+            List<Doador> doadores = repository.findAll();
+            return ResponseEntity.ok(doadores);
+        }
+
+        return ResponseEntity.noContent().build();
     }
 
     @GetMapping("/{idDoador}")
-    public ResponseEntity buscarUm(@PathVariable Integer idDoador){
+    public ResponseEntity buscarUm(
+            @PathVariable Integer idDoador,
+            HttpSession session
+    ) {
+        if (session.getAttribute("usuarioLogado") == null) {
+            return ResponseEntity.status(401).build();
+        }
+
         Optional<Doador> doadorOptional = repository.findById(idDoador);
         return ResponseEntity.of(doadorOptional);
     }
 
     @PostMapping()
-    public ResponseEntity criarDoador(@RequestBody @Valid Doador novoDoador) {
+    public ResponseEntity criarDoador(
+            @RequestBody @Valid Doador novoDoador,
+            HttpSession session
+    ) {
+        if (session.getAttribute("usuarioLogado") == null) {
+            return ResponseEntity.status(401).build();
+        }
+
         repository.save(novoDoador);
         return ResponseEntity.created(null).build();
     }
 
     @PutMapping("/{idDoador}")
-    public ResponseEntity alterarDoador(@PathVariable int idDoador,
-                                        @Valid @RequestBody Doador novoDoador){
-        if (repository.existsById(idDoador)){
+    public ResponseEntity alterarDoador(
+            @PathVariable int idDoador,
+            @Valid @RequestBody Doador novoDoador,
+            HttpSession session
+    ) {
+        if (session.getAttribute("usuarioLogado") == null) {
+            return ResponseEntity.status(401).build();
+        }
+
+        if (repository.existsById(idDoador)) {
             novoDoador.setId(idDoador);
             repository.save(novoDoador);
             return ResponseEntity.ok().build();
@@ -64,8 +80,15 @@ public class DoadoresController {
     }
 
     @DeleteMapping("/{idDoador}")
-    public ResponseEntity excluirDoador(@PathVariable int idDoador){
-        if (repository.existsById(idDoador)){
+    public ResponseEntity excluirDoador(
+            @PathVariable int idDoador,
+            HttpSession session
+    ) {
+        if (session.getAttribute("usuarioLogado") == null) {
+            return ResponseEntity.status(401).build();
+        }
+
+        if (repository.existsById(idDoador)) {
             repository.deleteById(idDoador);
             return ResponseEntity.ok().build();
         }
@@ -73,5 +96,23 @@ public class DoadoresController {
         return ResponseEntity.notFound().build();
     }
 
+//    @GetMapping()
+//    public ResponseEntity listar(@RequestParam(required = false) String tipoSanguineo) {
+//        Doador doador = new Doador(tipoSanguineo);
+//        List<Doador> doadoresFiltrados = repository.findAll(Example.of(doador));
+//        if (tipoSanguineo == null) {
+//            if (repository.count() > 0){
+//                return ResponseEntity.ok(repository.findAll());
+//            }else {
+//                return ResponseEntity.noContent().build();
+//            }
+//        } else {
+//            if (doadoresFiltrados.isEmpty()){
+//                return ResponseEntity.noContent().build();
+//            }else{
+//                return ResponseEntity.ok(doadoresFiltrados);
+//            }
+//        }
+//    }
 
 }
--- Blood4Life/back-end/spring-boot/blood-4-life/src/main/java/com/b4l/blood4life/controladores/HospitalController.java
+++ Blood4Life/back-end/spring-boot/blood-4-life/src/main/java/com/b4l/blood4life/controladores/HospitalController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 
 @RestController
@@ -16,32 +17,56 @@ public class HospitalController {
     HospitalRepository hospitalRepository;
 
     @GetMapping
-    public ResponseEntity getHospitais() {
+    public ResponseEntity getHospitais(
+            HttpSession session
+    ) {
+        if (session.getAttribute("usuarioLogado") == null) {
+            return ResponseEntity.status(401).build();
+        }
+
         return hospitalRepository.count() > 0
                 ? ResponseEntity.ok(hospitalRepository.findAll())
                 : ResponseEntity.noContent().build();
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity getHospitalById(@PathVariable Integer id) {
-        if (hospitalRepository.existsById(id)) {
-            return ResponseEntity.ok(hospitalRepository.findById(id));
+    public ResponseEntity getHospitalById(
+            @PathVariable Integer id,
+            HttpSession session
+    ) {
+        if (session.getAttribute("usuarioLogado") == null) {
+            return ResponseEntity.status(401).build();
         }
 
-        return ResponseEntity.notFound().build();
+        return hospitalRepository.existsById(id)
+                ? ResponseEntity.ok(hospitalRepository.findById(id))
+                : ResponseEntity.notFound().build();
     }
 
     @PostMapping
-    public ResponseEntity adicionaHospital(@RequestBody @Valid Hospital hospital) {
-        hospitalRepository.save(hospital);
+    public ResponseEntity adicionaHospital(
+            @RequestBody @Valid Hospital hospital,
+            HttpSession session
+    ) {
+        if (session.getAttribute("usuarioLogado") == null) {
+            return ResponseEntity.status(401).build();
+        }
 
+        hospitalRepository.save(hospital);
         return ResponseEntity.created(null).build();
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity editaHospital(@PathVariable Integer id,
-                                        @Valid @RequestBody Hospital hospital) {
-        if (hospitalRepository.existsById(id)){
+    public ResponseEntity editaHospital(
+            @PathVariable Integer id,
+            @Valid @RequestBody Hospital hospital,
+            HttpSession session
+    ) {
+        if (session.getAttribute("usuarioLogado") == null) {
+            return ResponseEntity.status(401).build();
+        }
+
+        if (hospitalRepository.existsById(id)) {
             hospital.setId(id);
             hospitalRepository.save(hospital);
             return ResponseEntity.ok().build();
@@ -51,8 +76,15 @@ public class HospitalController {
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity deletaHospital(@PathVariable Integer id) {
-        if (hospitalRepository.existsById(id)){
+    public ResponseEntity deletaHospital(
+            @PathVariable Integer id,
+            HttpSession session
+    ) {
+        if (session.getAttribute("usuarioLogado") == null) {
+            return ResponseEntity.status(401).build();
+        }
+
+        if (hospitalRepository.existsById(id)) {
             hospitalRepository.deleteById(id);
             return ResponseEntity.ok().build();
         }
