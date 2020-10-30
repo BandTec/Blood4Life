8896b5f72cfbab496a6060e3807832dcd2feef2e
ranyery <ranyerysc@gmail.com>
2020-10-29 23:46:54 -0300

    fix: update methods

--- Blood4Life/back-end/spring-boot/blood-4-life/src/main/java/com/b4l/blood4life/controladores/HospitalController.java
+++ Blood4Life/back-end/spring-boot/blood-4-life/src/main/java/com/b4l/blood4life/controladores/HospitalController.java
@@ -3,9 +3,11 @@ package com.b4l.blood4life.controladores;
 import com.b4l.blood4life.dominios.Hospital;
 import com.b4l.blood4life.servicos.HospitalService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 import java.util.List;
 
@@ -17,31 +19,35 @@ public class HospitalController {
     HospitalService hospitalService;
 
     @GetMapping
-    public ResponseEntity<List<Hospital>> getHospitais() {
-        return hospitalService.getAll();
+    public ResponseEntity<List<Hospital>> buscarTodosHospitais() {
+        return hospitalService.buscarTodos();
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Hospital> getHospitalById(@PathVariable Integer id) {
-        return hospitalService.getHospitalById(id);
+    public ResponseEntity<Hospital> buscarHospitalPeloId(@PathVariable Integer id) {
+        return hospitalService.buscarPeloId(id);
     }
 
     @PostMapping
-    public ResponseEntity<Hospital> adicionaHospital(@RequestBody @Valid Hospital hospital) {
-        return hospitalService.save(hospital);
+    public ResponseEntity<Hospital> adicionarHospital(
+            @RequestBody @Valid Hospital hospital,
+            HttpServletResponse response
+    ) {
+        Hospital hospitalAdicionado = hospitalService.adicionar(hospital, response);
+        return ResponseEntity.status(HttpStatus.CREATED).body(hospitalAdicionado);
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<Hospital> editaHospital(
+    public ResponseEntity<Hospital> atualizarHospitalPeloId(
             @PathVariable Integer id,
             @Valid @RequestBody Hospital hospital
     ) {
-        return hospitalService.update(id, hospital);
+        return hospitalService.atualizarPeloId(id, hospital);
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Hospital> deletaHospital(@PathVariable Integer id) {
-        return hospitalService.remove(id);
+    public ResponseEntity<Hospital> deletarHospitalPeloId(@PathVariable Integer id) {
+        return hospitalService.deletarPeloId(id);
     }
 
 }
--- Blood4Life/back-end/spring-boot/blood-4-life/src/main/java/com/b4l/blood4life/servicos/HospitalService.java
+++ Blood4Life/back-end/spring-boot/blood-4-life/src/main/java/com/b4l/blood4life/servicos/HospitalService.java
@@ -1,12 +1,15 @@
 package com.b4l.blood4life.servicos;
 
 import com.b4l.blood4life.dominios.Hospital;
+import com.b4l.blood4life.event.RecursoCriadoEvent;
 import com.b4l.blood4life.repositorios.HospitalRepository;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 
 @Service
@@ -15,7 +18,10 @@ public class HospitalService {
     @Autowired
     HospitalRepository hospitalRepository;
 
-    public ResponseEntity<List<Hospital>> getAll() {
+    @Autowired
+    private ApplicationEventPublisher publisher;
+
+    public ResponseEntity<List<Hospital>> buscarTodos() {
         List<Hospital> hospitais = hospitalRepository.findAll();
 
         if (hospitais.isEmpty()) {
@@ -25,7 +31,7 @@ public class HospitalService {
         return ResponseEntity.ok(hospitais);
     }
 
-    public ResponseEntity<Hospital> getHospitalById(Integer id) {
+    public ResponseEntity<Hospital> buscarPeloId(Integer id) {
         if (!hospitalRepository.existsById(id)) {
             return ResponseEntity.notFound().build();
         }
@@ -34,22 +40,27 @@ public class HospitalService {
         return ResponseEntity.ok(hospital);
     }
 
-    public ResponseEntity<Hospital> save(Hospital hospital) {
-        hospitalRepository.save(hospital);
-        return ResponseEntity.status(HttpStatus.CREATED).build();
+    public Hospital adicionar(Hospital hospital, HttpServletResponse response) {
+        Hospital hospitalSalvo = hospitalRepository.save(hospital);
+
+        publisher.publishEvent(
+                new RecursoCriadoEvent(this, response, hospitalSalvo.getId())
+        );
+
+        return hospitalSalvo;
     }
 
-    public ResponseEntity<Hospital> update(Integer id, Hospital hospital) {
+    public ResponseEntity<Hospital> atualizarPeloId(Integer id, Hospital hospital) {
         if (!hospitalRepository.existsById(id)) {
             return ResponseEntity.notFound().build();
         }
 
-        hospital.setId(id);
-        hospitalRepository.save(hospital);
+        Hospital hospitalSalvo = hospitalRepository.save(hospital);
+        BeanUtils.copyProperties(hospital, hospitalSalvo, "id");
         return ResponseEntity.ok().build();
     }
 
-    public ResponseEntity<Hospital> remove(Integer id) {
+    public ResponseEntity<Hospital> deletarPeloId(Integer id) {
         if (!hospitalRepository.existsById(id)) {
             return ResponseEntity.notFound().build();
         }
