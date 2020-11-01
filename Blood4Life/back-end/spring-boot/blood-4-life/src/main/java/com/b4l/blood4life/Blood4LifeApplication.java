b1bde27c114c7f4f659c545745dc32db32d03d24
ranyery <ranyerysc@gmail.com>
2020-11-01 17:11:13 -0300

    feat: add CORS

--- Blood4Life/back-end/spring-boot/blood-4-life/src/main/java/com/b4l/blood4life/Blood4LifeApplication.java
+++ Blood4Life/back-end/spring-boot/blood-4-life/src/main/java/com/b4l/blood4life/Blood4LifeApplication.java
@@ -2,9 +2,18 @@ package com.b4l.blood4life;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@EnableWebMvc
 @SpringBootApplication
-public class Blood4LifeApplication {
+public class Blood4LifeApplication implements WebMvcConfigurer {
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Blood4LifeApplication.class, args);
