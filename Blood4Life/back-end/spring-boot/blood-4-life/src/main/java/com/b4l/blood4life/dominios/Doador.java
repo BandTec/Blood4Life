7c7e16995fcd0ce035c513b85b1a429ed84f2702
ranyery <ranyerysc@gmail.com>
2020-11-01 14:27:24 -0300

    feat(Doador): add methods equals, hashCode and Builder

--- Blood4Life/back-end/spring-boot/blood-4-life/src/main/java/com/b4l/blood4life/dominios/Doador.java
+++ Blood4Life/back-end/spring-boot/blood-4-life/src/main/java/com/b4l/blood4life/dominios/Doador.java
@@ -9,7 +9,8 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.time.LocalDate;
+import java.util.Objects;
 
 @Entity
 public class Doador {
@@ -37,7 +38,7 @@ public class Doador {
 
     @Past
     @NotNull
-    private Date dtNascimento;
+    private LocalDate dtNascimento;
 
     @NotNull
     @Size(min = 10, max = 11)
@@ -48,7 +49,7 @@ public class Doador {
 
     private Character genero;
 
-    // Construtor default necessário para gerar erro na query
+    // Construtor default necessário para NÃO gerar erro na query
     public Doador() {
     }
 
@@ -60,6 +61,31 @@ public class Doador {
         this.senha = senha;
     }
 
+    private Doador(
+            Integer id,
+            @NotNull @Size(min = 2, max = 45) String nome,
+            @Email @NotNull @Size(min = 10, max = 60) String email,
+            @NotNull @Size(min = 8, max = 16) String senha,
+            @NotNull @Size(min = 11, max = 11) String cpf,
+            @Past @NotNull LocalDate dtNascimento,
+            @NotNull @Size(min = 10, max = 11) String telefone,
+            @Size(min = 2, max = 3) String tipoSanguineo,
+            Character genero
+    ) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+        this.senha = senha;
+        this.cpf = cpf;
+        this.dtNascimento = dtNascimento;
+        this.telefone = telefone;
+        this.tipoSanguineo = tipoSanguineo;
+        this.genero = genero;
+    }
+
+    public LocalDate getDtNascimento() {
+        return dtNascimento;
+    }
 
     public String formatarDataNascFormatada() {
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
@@ -108,12 +134,14 @@ public class Doador {
         this.cpf = cpf;
     }
 
-    public Date getDtNascimento() {
-        return dtNascimento;
+    public void setDtNascimento(LocalDate dtNascimento) {
+        this.dtNascimento = dtNascimento;
     }
 
-    public void setDtNascimento(Date dtNascimento) {
-        this.dtNascimento = dtNascimento;
+    @Override
+    public String toString() {
+        return String.format("01%-45s%-10s%11s%11s%-3s%n", getNome(), formatarDataNascFormatada(),
+                getCpf(), getTelefone(), getTipoSanguineo());
     }
 
     public String getTelefone() {
@@ -141,8 +169,76 @@ public class Doador {
     }
 
     @Override
-    public String toString() {
-        return String.format("01%-45s%-10s%11s%11s%-3s%n", getNome(),formatarDataNascFormatada(),
-                getCpf(), getTelefone(), getTipoSanguineo());
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Doador)) return false;
+        Doador doador = (Doador) o;
+        return Objects.equals(getId(), doador.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+
+    public static class Builder {
+        Integer id;
+        @NotNull @Size(min = 2, max = 45) String nome;
+        @Email @NotNull @Size(min = 10, max = 60) String email;
+        @NotNull @Size(min = 8, max = 16) String senha;
+        @NotNull @Size(min = 11, max = 11) String cpf;
+        @Past @NotNull LocalDate dtNascimento;
+        @NotNull @Size(min = 10, max = 11) String telefone;
+        @Size(min = 2, max = 3) String tipoSanguineo;
+        Character genero;
+
+        public Builder setId(Integer id) {
+            this.id = id;
+            return this;
+        }
+
+        public Builder setNome(String nome) {
+            this.nome = nome;
+            return this;
+        }
+
+        public Builder setEmail(String email) {
+            this.email = email;
+            return this;
+        }
+
+        public Builder setSenha(String senha) {
+            this.senha = senha;
+            return this;
+        }
+
+        public Builder setCpf(String cpf) {
+            this.cpf = cpf;
+            return this;
+        }
+
+        public Builder setDtNascimento(LocalDate dtNascimento) {
+            this.dtNascimento = dtNascimento;
+            return this;
+        }
+
+        public Builder setTelefone(String telefone) {
+            this.telefone = telefone;
+            return this;
+        }
+
+        public Builder setTipoSanguineo(String tipoSanguineo) {
+            this.tipoSanguineo = tipoSanguineo;
+            return this;
+        }
+
+        public Builder setGenero(Character genero) {
+            this.genero = genero;
+            return this;
+        }
+
+        public Doador build() {
+            return new Doador(id, nome, email, senha, cpf, dtNascimento, telefone, tipoSanguineo, genero);
+        }
     }
 }
