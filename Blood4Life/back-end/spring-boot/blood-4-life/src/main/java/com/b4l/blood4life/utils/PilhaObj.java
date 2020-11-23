316e645f764a294a8191bf118b8542b0951e3499
raphaelbachega <raphael.messias@bandtec.com.br>
2020-11-23 18:52:27 -0300

    Create pilhaObj class

---
+++ Blood4Life/back-end/spring-boot/blood-4-life/src/main/java/com/b4l/blood4life/utils/PilhaObj.java
@@ -0,0 +1,53 @@
+package com.b4l.blood4life.utils;
+
+public class PilhaObj<T> {
+    private int topo;
+    private final T[] pilha;
+
+    public PilhaObj(int capacidade) {
+        topo = -1;
+        pilha = (T[]) new Object[capacidade];
+    }
+
+    public boolean isEmpty() {
+        return topo == -1;
+    }
+
+    public boolean isFull() {
+        return (topo == pilha.length - 1);
+    }
+
+    public void push(T info) {
+        if (!isFull()) {
+            pilha[++topo] = info;
+        } else {
+            System.out.println("Pilha cheia");
+        }
+    }
+
+    public T pop() {
+        if (!isEmpty()) {
+            return pilha[topo--];
+        }
+        return null;
+    }
+
+    public T peek() {
+        if (!isEmpty()) {
+            return pilha[topo];
+        }
+        return null;
+    }
+
+    public void exibe() {
+        if (isEmpty()) {
+            System.out.println("Pilha vazia");
+        } else {
+            for (int i = 0; i <= topo; i++) {
+                System.out.println(pilha[i]);
+            }
+        }
+
+    }
+
+}
