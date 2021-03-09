5f039d8534c957fdb715775b2a7eaf31418afaac
raphaelbachega <53584067+raphaelbachega@users.noreply.github.com>
2021-03-09 20:23:40 -0300

    Update api.js

--- Blood4Life/front-end/react-blood4life/src/services/api.js
+++ Blood4Life/front-end/react-blood4life/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
     //baseUrl: process.env.NODE_ENV === 'developmente' ? 'http://localhost:8000' : 'https://nodeblood.azurewebsites.net'
-     baseURL: "http://localhost:8080"
+     baseURL: "http://52.54.66.63:8080/"
 });
 
 export default api;
